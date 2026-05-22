9b949b52dfeee8b8edf8af25ec277a3c8d41470f
Michael Borisov <michael.borisov@web.de>
2026-05-22 22:32:38 +0200

    Initial LazyVim config
    LazyVim starter with provider warnings silenced in options.lua and
    extras enabled for rust, python, typescript, json, toml, tailwind,
    clangd, cmake, sql, git, markdown plus QoL plugins.

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable unused legacy remote-plugin providers (silences :checkhealth warnings).
+-- These do not affect editing any language -- LSP, Treesitter and Mason tools
+-- run independently of these providers.
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
