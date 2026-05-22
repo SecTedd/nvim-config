1092d6f622b9aed1a12a4a02def9567bdc408b75
Michael Borisov <michael.borisov@web.de>
2026-05-22 23:25:44 +0200

    zed config import
    took settings + keymaps from zed vim plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Exit insert mode with jk
+map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Splits (splitright/splitbelow are on by default, so these open right/down)
+map("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "Split window right" })
+map("n", "<leader>h", "<cmd>split<cr>", { desc = "Split window down" })
+
+-- Format buffer
+map({ "n", "v" }, "<leader>p", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format buffer" })
