456b59bf870410b3a6c549b748ec6ada60544e7f
Michael Borisov <michael.borisov@web.de>
2026-05-26 00:45:35 +0200

    config tweaks
    removed the example.lua file
    removed redundant custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,12 +6,3 @@ local map = vim.keymap.set
 
 -- Exit insert mode with jk
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-
--- Splits (splitright/splitbelow are on by default, so these open right/down)
-map("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "Split window right" })
-map("n", "<leader>h", "<cmd>split<cr>", { desc = "Split window down" })
-
--- Format buffer
-map({ "n", "v" }, "<leader>p", function()
-  LazyVim.format({ force = true })
-end, { desc = "Format buffer" })
